d1d0cb3310ec4bacc86143c11bc309afe36dbe0d
MarcerCyoon <4866635+MarcerCyoon@users.noreply.github.com>
2021-08-20 17:25:30 -0400

    more events fix

--- autojson.py
+++ autojson.py
@@ -211,6 +211,7 @@ def get_bird_rights_team(player, events, currentYear, teams, threshold=3):
     # cannot have birds.
     # Filter out all non-player events and filter for events with specified player.
     player_events = list(filter(lambda event: event['type'] not in ['newLeague', 'playoffs', 'madePlayoffs', 'newTeam', 'draftLottery', 'teamExpansion', 'gameAttribute', 'teamRelocation', 'teamContraction'] and player['pid'] in event['pids'], events))
+    player_events = list(filter(lambda event: event['type'] not in ['newLeague', 'playoffs', 'madePlayoffs', 'newTeam', 'draftLottery', 'teamExpansion', 'gameAttribute', 'teamRelocation', 'teamContraction', 'teamLogo'] and player['pid'] in event['pids'], events))
 
     if len(player_events) == 0:
     	return None
